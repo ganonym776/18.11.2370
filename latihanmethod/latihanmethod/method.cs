6274a191701bd1bc4cc4a4ed17a357be39cc431f
ganonym776 <gunawan@mail.com>
2019-03-26 12:19:31 +0700

    taksii

---
+++ latihanmethod/latihanmethod/method.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmethod
+{
+    class method
+    {
+        public int Penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+        public int Penjumlah(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+        public int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
