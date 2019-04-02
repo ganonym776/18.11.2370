28eb1365627e9fa0dc6ced0a0a28adaf2a082842
gunawan <gunawan@mail.com>
2019-04-02 12:16:23 +0700

    18.11.2370

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihanmethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarasi variable
+            int hasil;
+
+            //membuat objek dari class calculator
+            Kalkulator kalkulator = new Kalkulator();
+
+            //penggil method penjumlah
+            hasil = kalkulator.Penjumlah(10, 2);
+            CetakHasil("Hasil Penjumlahan = " + hasil);
+
+            //panggil method penjumlah dengan 3 parameter
+            hasil = kalkulator.Penjumlah(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan = " + hasil);
+
+            //panggil method pengurangan
+            hasil = kalkulator.Pengurangan(7, 2);
+            CetakHasil("Hasil Pengurangan = " + hasil);
+
+            //panggil static method, langsung dari nama classnya
+            hasil = Kalkulator.Perkalian(5, 2);
+            CetakHasil("Hasil Perkalian = " + hasil);
+
+            Console.ReadKey();
+        }
+        static void CetakHasil(string hasil)
+        {
+            Console.WriteLine(hasil);
+        }
+    }
+}
---
+++ Constructor/Constructor/Mahasiswa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Mahasiswa
+    {
+        public Mahasiswa()
+        {
+        }
+
+        //constructor dengan 2 parameter
+        public Mahasiswa(string nim, string nama)
+        {
+            Nim = nim;
+            Nama = nama;
+        }
+
+        //properties
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+    }
+}
